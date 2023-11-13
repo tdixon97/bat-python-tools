0f89f6421da0de7cf1aadac76883769d27345fc2
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2023-11-13 19:08:17 +0100

    switch to setting styles in a json file

--- plot-taup.py
+++ plot-taup.py
@@ -11,6 +11,7 @@ import hist
 import argparse
 import re
 import utils
+import json
 
 vset = tc.tol_cset('vibrant')
 mset = tc.tol_cset('muted')
@@ -23,98 +24,16 @@ style = {
     "lw": 0.8,
 }
 
-components = OrderedDict({
-    "2vbb": {
-        "hists": ["Nvbb_bege", "Nvbb_icpc", "Nvbb_coax", "Nvbb_ppc"],
-        "style": {
-            "color": mset.olive,
-            "label": r"$2\nu\beta\beta$",
-        }
-    },
-    "Th": {
-        "hists": [
-            'Ac228_pen_plates',
-            'Bi212Tl208_birds_nest',
-            'Bi212Tl208_cables',
-            'Bi212Tl208_fiber_shroud',
-            'Bi212Tl208_front_end_electronics',
-            'Bi212Tl208_hpge_support_copper',
-            'Bi212Tl208_mini_shroud',
-            'Bi212Tl208_pen_plates',
-            'Bi212Tl208_wls_reflector',
-        ],
-        "style": {
-            "color": vset.cyan,
-            "label": "Thorium",
-        }
-    },
-    "U": {
-        "hists": [
-            'Pb214Bi214_birds_nest',
-            'Pb214Bi214_cables',
-            'Pb214Bi214_fiber_shroud',
-            'Pb214Bi214_front_end_electronics',
-            'Pb214Bi214_hpge_support_copper',
-            'Pb214Bi214_mini_shroud',
-            'Pb214Bi214_pen_plates',
-            'Pb214Bi214_wls_reflector',
-        ],
-        "style": {
-            "color": vset.teal,
-            "label": "Uranium",
-        }
-    },
-    "K40": {
-        "hists": [
-            'K40_front_end_electronics',
-            'K40_hpge_support_copper',
-            'K40_pen_plates',
-            'K40_wls_reflector',
-        ],
-        "style": {
-            "color": vset.red,
-            "label": "$^{40}$K"
-        }
-    },
-    "K42": {
-        "hists": [
-            'K42_homogeneous',
-            'K42_hpge_shell',
-            'K42_hpge_surface',
-            'K42_mini_shroud',
-        ],
-        "style": {
-            "color": vset.magenta,
-            "label": "$^{42}$K"
-        }
-    },
-    "total_model": {
-        "hists": ["total_model"],
-        "style": {
-            "label": "Model",
-            "color": vset.black,
-        }
-    },
-    "data": {
-        "hists": ["fitted_data"],
-        "style": {
-            "label": "Data",
-            # "color": vset.black,
-            # "histtype": "errorbar",
-            # "markersize": 3,
-            #"linewidth": 0,
-            "histtype": "fill",
-            "color": vset.blue,
-            "alpha": 0.25,
-        }
-    },
-})
+
+
+
 parser = argparse.ArgumentParser(description='A script with command-line argument.')
 parser.add_argument('-d','--det_type', type=str, help='Detector type',default='all')
 parser.add_argument('-l','--lower_x', type=int, help='Low range',default=565)
 parser.add_argument('-u','--upper_x', type=int, help='Upper range',default=3000)
 parser.add_argument("-s","--scale",type=str,help="Scale",default="log")
 parser.add_argument("-o","--out_file",type=str,help="file",default="../results/hmixfit-l200a-taup-silver-dataset-m1-histograms.root")
+parser.add_argument("-c","--components",type=str,help="json components config file path",default="components.json")
 
 
 args = parser.parse_args()
@@ -126,8 +45,13 @@ scale=args.scale
 outfile=args.out_file
 first_index =utils.find_and_capture(outfile,"hmixfit-")
 name_out = outfile[first_index:-16]
+json_file =args.components
+
+with open(json_file, 'r') as file:
+    components = json.load(file, object_pairs_hook=OrderedDict)
+
 def get_hist(obj):
-    return obj.to_hist()[552:2982][hist.rebin(15)]
+    return obj.to_hist()[200:2982][hist.rebin(15)]
 
 if det_type=="all":
     datasets = ["l200a_taup_silver_dataset_bege", "l200a_taup_silver_dataset_icpc","l200a_taup_silver_dataset_ppc","l200a_taup_silver_dataset_coax"]
