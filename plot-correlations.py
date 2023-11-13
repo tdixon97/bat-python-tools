0e9a83399dde0574a7bcd04b2eb7564b44993b96
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2023-11-13 19:17:12 +0100

    initial commit of plot-correlations script

---
+++ plot-correlations.py
@@ -0,0 +1,27 @@
+from legend_plot_style import LEGENDPlotStyle as lps
+lps.use('legend')
+
+from collections import OrderedDict
+import uproot
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import tol_colors as tc
+import hist
+import argparse
+import re
+import utils
+import json
+
+vset = tc.tol_cset('vibrant')
+mset = tc.tol_cset('muted')
+plt.rc('axes', prop_cycle=plt.cycler('color', list(vset)))
+
+
+style = {
+    "yerr": False,
+    "flow": None,
+    "lw": 0.8,
+}
+
+
