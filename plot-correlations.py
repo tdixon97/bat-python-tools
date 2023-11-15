fc22ab124a9feb2e94e1da3d78bf5141a068b017
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2023-11-15 17:42:52 +0100

    updating plot-correaltion to read mcmc

--- plot-correlations.py
+++ plot-correlations.py
@@ -12,16 +12,65 @@ import argparse
 import re
 import utils
 import json
+import mplhep
 
 vset = tc.tol_cset('vibrant')
 mset = tc.tol_cset('muted')
 plt.rc('axes', prop_cycle=plt.cycler('color', list(vset)))
 
 
-style = {
+style_1d = {
     "yerr": False,
     "flow": None,
     "lw": 0.8,
 }
 
 
+style_2d = {
+    "flow": None,
+    "lw": 0.8,
+    "cmin":1,
+    "cmap":"cividis"
+
+}
+parser = argparse.ArgumentParser(description='A script with command-line argument.')
+parser.add_argument("-o","--out_file",type=str,help="file",default="../hmixfit/results/hmixfit-l200a_taup_silver_dataset_m1_norm-_mcmc.root")
+parser.add_argument("-c","--components",type=str,help="json components config file path",default="components.json")
+parser.add_argument("-t","--tree_name",type=str,help="tree name",default="l200a_taup_silver_dataset_m1_norm")
+args = parser.parse_args()
+
+outfile=args.out_file
+tree_name = args.tree_name
+
+
+
+tree= "{}_mcmc".format(tree_name)
+df =utils.ttree2df(outfile,tree)
+df=df.query("Phase==1")
+
+print(df)
+x=np.array(df["Pb214Bi214_pen_plates"])
+y=np.array(df["Pb214Bi214_wls_reflector"])
+rangex=(min(x),max(x))
+rangey=(min(y),max(y))
+bins=(100,100)
+utils.plot_two_dim(x,y,rangex,rangey,"$^{214}$Pb+Bi pen plates [decays/yr]","$^{214}$Pb+Bi wls reflector [decays/yr]","",bins)
+"""
+with uproot.open(outfile) as f:
+    
+    for key in f.keys():
+        histo =f[key].to_hist()
+        
+
+        fig, axes = lps.subplots(1, 1, figsize=(6, 4), sharex=True)
+        print(key)
+        print(key[0]+key[1])
+       
+        if (key[0]+key[1]=="h1"):
+            histo.plot(ax=axes,**style_1d)
+        else:
+            histo.plot2d(ax=axes,**style_2d)
+        substring = key[:-2]
+        plt.savefig("plots"+"/"+substring+".pdf")
+
+"""
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import re
-
-
+import pandas as pd
+import uproot
+from legend_plot_style import LEGENDPlotStyle as lps
+lps.use('legend')
+import matplotlib.pyplot as plt
+import numpy as np
 
 def find_and_capture(text:str, pattern:str):
     """Function to  find the start index of a pattern in a string
@@ -18,3 +22,95 @@ def find_and_capture(text:str, pattern:str):
     else:
         raise ValueError("pattern {} not found in text {} ".format(pattern,text))
     
+def manipulate_title(text:str):
+    """Replace all the # with \  and add $ $"""
+
+    
+    new_string=""
+
+    for char in text:
+        if (char=="#"):
+            new_string+="$\\"
+        
+
+def format_latex(list_str):
+    list_new=[]
+    for text in list_str:
+        modified_string =text.replace("Pb214", "$^{214}$Pb")
+        modified_string=modified_string.replace("Bi214","$^{214}$Bi")
+        modified_string=modified_string.replace("Tl208","$^{214}$Bi")
+        modified_string=modified_string.replace("K40","$^{40}$K")
+        modified_string=modified_string.replace("K42","$^{42}$K")
+        modified_string=modified_string.replace("Bi212","$^{212}$Bi")
+
+        list_new.append(modified_string)
+    return list_new
+    
+
+def ttree2df(filename:str,tree_name:str)->pd.DataFrame:
+    """Use uproot to import a TTree and save to a dataframe
+    Parameters:
+        - filename: file to open
+        - tree_name: Which TTree to look at
+    Returns:
+        Pandas DataFrame of the data
+    """
+
+    file = uproot.open(filename)
+
+    # Access the TTree inside the file
+    tree = file[tree_name]
+
+    # Get the list of branches in the TTree
+    branches = tree.keys()
+
+    # Define a dictionary to store the branch data
+    data = {}
+
+    # Loop through each branch and extract the data
+
+    for branch_name in branches:
+        # Use uproot to read the branch into a NumPy array
+        data[branch_name] = tree[branch_name].array()
+
+    # Create a Pandas DataFrame from the dictionary
+    return pd.DataFrame(data)
+
+
+
+
+
+def plot_two_dim(varx:np.ndarray,vary:np.ndarray,rangex:tuple,rangey:tuple,titlex:str,titley:str,title:str,bins:tuple):
+    """
+    A 2D scatter plot
+    Parameters:
+        - varx: Numpy array of the x data
+        - vary: Numpy array of the y data
+        - rangex: tuple (low,high) for range of the x axis
+        - rangey: tuple (low,high) for range of the y axis
+        - titlex: Title for x-axis
+        - titley: Title for the y-axis
+        - title: Plot title
+        - bins: Tuple of (binsx,binsy)
+    Returns:
+        None
+    """
+
+    ## create the axis
+
+    fig, axes = lps.subplots(1, 1, figsize=(6, 4), sharex=True, gridspec_kw = {'hspace': 0})
+
+    h = axes.hist2d(varx,vary, bins=bins, cmap='viridis', range=[rangex,rangey], cmin=1)
+
+    fig.colorbar(h[3], ax=axes, label='Counts')
+    axes.set_xlabel(titlex)
+    axes.set_ylabel(titley)
+    axes.set_title(title)
+
+    plt.show()
+
+    correlation_coefficient = np.corrcoef(varx, vary)[0, 1]
+
+    # Annotate the plot with correlation coefficient
+    correlation_text = f'Correlation: {correlation_coefficient:.2f}'
+    axes.annotate(correlation_text, xy=(0.7, 0.95), xycoords='axes fraction', fontsize=10, color='white')
