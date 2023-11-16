1967c75234aef64f4f5d0960756a4f3947c9dacf
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2023-11-16 13:19:31 +0100

    switch to df.corr() to compute correlation matrix

--- plot-correlations.py
+++ plot-correlations.py
@@ -38,11 +38,13 @@ parser = argparse.ArgumentParser(description='A script with command-line argumen
 parser.add_argument("-o","--out_file",type=str,help="file",default="../hmixfit/results/hmixfit-l200a_taup_silver_dataset_m1_norm-_mcmc.root")
 parser.add_argument("-c","--components",type=str,help="json components config file path",default="components.json")
 parser.add_argument("-t","--tree_name",type=str,help="tree name",default="l200a_taup_silver_dataset_m1_norm")
+parser.add_argument("-m","--make_plots",type=bool,help="make scatter plots?",default=False)
+
 args = parser.parse_args()
 
 outfile=args.out_file
 tree_name = args.tree_name
-
+make_plots=args.make_plots
 
 
 tree= "{}_mcmc".format(tree_name)
@@ -119,33 +121,33 @@ matrix = np.zeros(shape=(len(df.keys()),len(df.keys())))
 labels=utils.format_latex(df.keys())
 i=0
 j=0
-for key1 in df.keys():
-    i=0
-    for key2 in df.keys():
-        if (i>j):
-            continue
-        x=np.array(df[key1])
-        y=np.array(df[key2])
-        rangex=(0,max(x))
-        rangey=(0,max(y))
-        bins=(100,100)
-        if (key1!=key2):
-            cor=utils.plot_two_dim(x,y,rangex,rangey,"{} [1/yr]".format(labels[i]),
-                                                 "{} [1/yr]".format(labels[j]),
-                                                 "{} vs {}".format(labels[i],labels[j]),bins)
-        else:
-            cor=1
-        
-        matrix[i,j]=cor
-        matrix[j,i]=cor
+matrix = df.corr()
 
-        #print("Correlation {} to {} = {"0}")
-        i+=1
-    j+=1
+if (make_plots==True):
+    for key1 in df.keys():
+        i=0
+        for key2 in df.keys():
+            if (i>j):
+                continue
+            x=np.array(df[key1])
+            y=np.array(df[key2])
+            rangex=(0,max(x))
+            rangey=(0,max(y))
+            bins=(100,100)
+            if (key1!=key2):
+                cor=utils.plot_two_dim(x,y,rangex,rangey,"{} [1/yr]".format(labels[i]),
+                                                    "{} [1/yr]".format(labels[j]),
+                                                    "{} vs {}".format(labels[i],labels[j]),bins)
+            else:
+                cor=1
+            
+            i+=1
+        j+=1
 
 utils.plot_correlation_matrix(matrix,"","plots/Full_matrix_{}.pdf".format(tree_name))
 
 utils.plot_correlation_matrix(matrix[index_U,index_U],"","plots/U_matrix_{}.pdf".format(tree_name))
+print(matrix[index_U,index_U])
 utils.plot_correlation_matrix(matrix[index_Th,index_Th],"","plots/Th_matrix_{}.pdf".format(tree_name))
 utils.plot_correlation_matrix(matrix[index_K40,index_K40],"","plots/K40_matrix_{}.pdf".format(tree_name))
 utils.plot_correlation_matrix(matrix[index_K42,index_K42],"","plots/K42_matrix_{}.pdf".format(tree_name))
