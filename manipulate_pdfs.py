1e9ab349862abb5a409faf2d71e775c862e281aa
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-01-15 17:59:37 +0000

    initialise manipulate_pdfs scripts

---
+++ manipulate_pdfs.py
@@ -0,0 +1,5 @@
+"""
+manipulate_pdfs.py
+author: Toby Dixon (toby.dixon.23@ucl.ac.uk)
+Make a manipulation of the pdfs for the fit (data and mc are treated symmetrically) to produce new fit pdfs
+"""
