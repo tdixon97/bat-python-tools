d2c79be21c03c90336c826e8f907ca6b62392318
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2024-03-12 15:50:22 +0000

    arge parser for plot-correlations

--- plot-correlations.py
+++ plot-correlations.py
@@ -1,3 +1,8 @@
+""" A script to plot the correlation matrix from the hmixfit fit
+Author: Toby Dixon (toby.dixon.23@ucl.ac.uk)
+
+"""
+
 from legend_plot_style import LEGENDPlotStyle as lps
 lps.use('legend')
 
@@ -9,6 +14,7 @@ import numpy as np
 import tol_colors as tc
 import hist
 import argparse
+import os
 import re
 import utils
 import json
@@ -36,25 +42,33 @@ style_2d = {
 
 }
 parser = argparse.ArgumentParser(description='A script with command-line argument.')
-parser.add_argument("-o","--out_file",type=str,help="file",default="../hmixfit/results/hmixfit-l200a_taup_silver_dataset_m1_norm-_mcmc.root")
-parser.add_argument("-c","--components",type=str,help="json components config file path",default="components.json")
-parser.add_argument("-t","--tree_name",type=str,help="tree name",default="l200a_taup_silver_dataset_m1_norm")
-parser.add_argument("-m","--make_plots",type=bool,help="make scatter plots?",default=False)
+parser.add_argument("-c","--cfg",type=str,help="The configuration file for the hmixfit fit")
+parser.add_argument("-C","--components",type=str,help="json components config file path",default="components.json")
+parser.add_argument("-m","--make_plots",type=bool,help="make scatter plots?",default=True)
+parser.add_argument("-N","--step",type=int,help="Number of steps of the markov chain to take ",default=100000)
+parser.add_argument("-O","--outdir",type=str,help="output directory to save plots",default="plots/summary/")
 
 args = parser.parse_args()
 
-outfile=args.out_file
-tree_name = args.tree_name
+N=args.step
 make_plots=args.make_plots
+cfg_path =args.cfg
+outdir=args.outdir
 
+with open(cfg_path,"r") as file:
+    cfg =json.load(file)
 
-df =utils.ttree2df(outfile,"mcmc")
+### extract all we need from the cfg dict
+fit_name,out_dir,_,_,dataset_name=utils.parse_cfg(cfg)
+outfile = out_dir+"/hmixfit-"+fit_name+"/mcmc_small.root"
+os.makedirs(outdir+"/"+fit_name,exist_ok=True)
+
+tree_name= "{}_mcmc".format(fit_name)
+df =utils.ttree2df(outfile,"mcmc","Phase==1",N=N)
+
 df=df.query("Phase==1")
-
-
 df=df.drop(columns=['Chain','Iteration','Phase','LogProbability','LogLikelihood','LogPrior','Ar39_homogeneous'])
 
-print(df)
 ### make the name key
 names={"Index":[],"Name":[]}
 names_U={"Index":[],"Name":[]}
@@ -74,39 +88,20 @@ for key in df.keys():
     names["Name"].append(labels[i])
     if (key.find("Bi212")!=-1):
         names_Th['Name'].append(labels[i])
-        names_Th["Index"].append(len(index_U))
+        names_Th["Index"].append(len(index_Th))
         index_Th.append(i)
     elif(key.find("Bi214")!=-1):
         names_U['Name'].append(labels[i])
-        names_U["Index"].append(len(index_Th))
+        names_U["Index"].append(len(index_U))
         index_U.append(i)
     elif(key.find("K")!=-1):
         names_K['Name'].append(labels[i])
         names_K["Index"].append(len(index_K))
         index_K.append(i)
-   
-        
-    
+      
     i=i+1
-df_key = pd.DataFrame(names)
-utils.plot_table(df_key,"plots/fit_correlations/key_{}.pdf".format(tree_name))
-
-df_key_U = pd.DataFrame(names_U)
-utils.plot_table(df_key_U,"plots/fit_correlations/key_U_{}.pdf".format(tree_name))
-
-df_key_Th = pd.DataFrame(names_Th)
-utils.plot_table(df_key_Th,"plots/fit_correlations/key_Th_{}.pdf".format(tree_name))
-
-df_key_K = pd.DataFrame(names_K)
-utils.plot_table(df_key_K,"plots/fit_correlations/key_K_{}.pdf".format(tree_name))
-
 
 ### make the full correlation matrix and subplots
-##1) 2vbb rho plot
-##2) 212Bi + 208Tl
-##3) 214Bi + 214 Bi
-##4) 40K
-###5) 42K
 matrix = np.zeros(shape=(len(df.keys()),len(df.keys())))
 
 ### make the full matrix
@@ -115,38 +110,32 @@ i=0
 j=0
 matrix = df.corr()
 
-if (make_plots==True):
-    for key1 in df.keys():
-        i=0
-        for key2 in df.keys():
-            if (i>j):
-                continue
-            x=np.array(df[key1])
-            y=np.array(df[key2])
-            rangex=(0,max(x))
-            rangey=(0,max(y))
-            bins=(100,100)
-            if (key1!=key2):
-                cor=utils.plot_two_dim(x,y,rangex,rangey,"{} [1/yr]".format(labels[i]),
-                                                    "{} [1/yr]".format(labels[j]),
-                                                    "{} vs {}".format(labels[i],labels[j]),bins)
-            else:
-                cor=1
-            
-            i+=1
-        j+=1
-        
 matrix=np.array(matrix)
-with PdfPages('plots/fit_correlations/corrs.pdf') as pdf:
+with PdfPages('{}/{}/highest_correlations.pdf'.format(outdir,fit_name)) as pdf:
 
-    for n in range(100):
+
+    ### 10 highest overall
+    for n in range(20):
         cor,i,j = utils.get_nth_largest(np.abs(matrix),n)
-        
-        print("{}: {} to {} = {:0.2f} ".format(n,labels[i],labels[j],cor))
-        utils.plot_corr(df,i,j,labels,"plots/fit_correlations/Correlation_{}_to_{}.pdf".format(labels[i],labels[j]),pdf=pdf)
-utils.plot_correlation_matrix(matrix,"","plots/fit_correlations/Full_matrix_{}.pdf".format(tree_name),show=True)
-utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_U),"","plots/fit_correlations/Matrix_U_{}.pdf".format(tree_name))
-utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_Th),"","plots/fit_correlations/Matrix_Th_{}.pdf".format(tree_name))
-utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_K),"","plots/fit_correlations/Matrix_K_{}.pdf".format(tree_name))
+        utils.plot_corr(df,i,j,labels,pdf=pdf)
+
+
+with PdfPages('{}/{}/correlation_matrix.pdf'.format(outdir,fit_name)) as pdf:
+
+    df_key = pd.DataFrame(names)
+    utils.plot_table(df_key,pdf)
+    utils.plot_correlation_matrix(matrix,"",pdf,show=False)
+
+    df_key_U = pd.DataFrame(names_U)
+    utils.plot_table(df_key_U,pdf)
+    utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_U),"",pdf)
+
+    df_key_Th = pd.DataFrame(names_Th)
+    utils.plot_table(df_key_Th,pdf)
+    utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_Th),"",pdf)
+
+    df_key_K = pd.DataFrame(names_K)
+    utils.plot_table(df_key_K,pdf)
+    utils.plot_correlation_matrix(utils.twoD_slice(matrix,index_K),"",pdf)
 
 
